fb6454659b969de06c2c107bcce4c4323598e56c
shashank833 <shashank.rangaraj@synergysolutions.asia>
2018-05-30 15:57:06 +0530

    commit_2

--- SampleProject_HelloWorld/src/HelloWorld.java
+++ SampleProject_HelloWorld/src/HelloWorld.java
@@ -2,6 +2,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hellow World");
-		System.out.println("new_shashank");
+		System.out.println("new_shashank_1");
+		System.out.println("new_shashank_2");
 	}
 }
